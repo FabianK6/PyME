5e8c644ccd712e7cda73afcf3dfcc3f40c3dce23
Fabian Koch <fabian.koch6@outlook.com>
2026-06-24 07:18:01 +0200

    import is bit wanky but i will figure it out. trying import . bla as bla

--- PyME/PyME/__init__.py
+++ PyME/PyME/__init__.py
@@ -6,13 +6,12 @@ Created on Mon May 18 23:17:52 2026
 @author: fabian
 """
 
-from .bearings import *
-from .gears import *
-from .materials import *
-from .pins import *
-from .screws import *
-from .shafts import *
-from .springs import *
-
+from . import bearings
+from . import gears
+from . import materials
+from . import pins
+from . import screws
+from . import shafts
+from . import springs
 
 __version__ = "0.1.0"
