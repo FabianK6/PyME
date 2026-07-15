53de5a0088ca057282dcbf0fc2fc661bedab6cce
Fabian Koch <fabian.koch6@outlook.com>
2026-07-15 19:59:27 +0200

    toothbelts ready for testing

--- PyME/PyME/belts.py
+++ PyME/PyME/belts.py
@@ -1,10 +1,22 @@
 import numpy as np
 
-from MT22ta.Admir_Filan.ME_kraefte_kettentrieb_filanadm import Ft
-
 _TROLLEY_DIAMETER_ = np.array([
     [0.00075, 0.0009, 0.001, 0.0016, 0.0018, 0.003, 0.0045, 0.008, 0.01, 0.015, 0.04, 0.06, 0.1, 0.12,  0.14, 0.17, 0.2, 0.25, 0.3, 0.4, 0.44],
     [63, 71, 80, 90, 100, 112, 125, 140, 160, 180, 200, 224, 250, 280, 315, 355, 400, 450, 500, 560, 630]
+])
+_TOOTHBELT_PARAMS_ = np.array([
+    # teilung, zahnhöhe, min Länge, max Länge, min Scheiben-Zähnezahl, max Scheiben-Zähnezahl, min Zähnezahl bei gegenbiegung
+    [2.5, 0.7, 120, 1475, 10, 114, 11, 0.5, 20000, 80],
+    [5, 1.2, 100, 1500, 10, 114, 12, 5, 10000, 80],
+    [10, 2.5, 260, 3620, 15, 114, 20, 30, 10000, 60],
+    [20, 5, 1260, 3620, 15, 114, 20, 100, 6500, 40]
+])
+_TOOTHBELT_FORCE_ = np.array([
+    [4, 6, 10, 16, 25, 32, 50, 75, 100, 150],
+    [39, 65, 117, 195, 312, 403, 0, 0, 0, 0],
+    [0, 150, 300, 510, 870, 1100, 1800, 2730, 3660],
+    [0, 0, 0, 1200, 2000, 2700, 4300, 6600,  8800, 13400],
+    [0, 0, 0, 0, 0, 4750, 7750, 12000, 16000, 24500]
 ])
 
 class Belt(object):
@@ -31,23 +43,58 @@ class ToothBelt(Belt):
         ratio: float|int,
         app_factor: float|int
     ):
+        # rotationsgeschwindigkeit der Antriebsseite
         self.angular_velocity = num_of_rev / 60 * 2 * np.pi
+        # zu übertragende Leistung
         self.power = torque * self.angular_velocity * app_factor
+        # P / n verhältnis für  die Wahl der Raddurchmesser
         ptn = self.power/1000 / num_of_rev
         index = np.where(_TROLLEY_DIAMETER_[0] >= ptn)
+        # Durchmesser des treibenden Rads
         d1 = _TROLLEY_DIAMETER_[1, index[0]]
+        # Durchmesser des getriebenen Rads
         d2 = d1 * ratio
         super().__init__(torque, num_of_rev, app_factor, d1, d2)
 
-    def calculate_toothbelt_length(self):
+    def step_1_calc_toothbelt_length(self):
         v = self.trolley_diameter_drive * np.pi * self.num_of_rev
-        Ft = self.power / v
+        self.tangential_force = self.power / v
         dm = (self.trolley_diameter_drive + self.trolley_diameter_load) * 0.5
         e_ = (15 + dm + 2 * (self.trolley_diameter_drive + self.trolley_diameter_load)) / 2
         Ld_ = 2 * e_ + np.pi/2 * (self.trolley_diameter_drive + self.trolley_diameter_load) + (self.trolley_diameter_drive + self.trolley_diameter_load)**2 / (4 * e_)
+        return e_, Ld_
 
+
+    def step_2_calc_shaft_distance(self, belt_length: float|int):
+        self.belt_length = belt_length
+        self.e = belt_length / 4 * np.pi / 8 * (self.trolley_diameter_drive + self.trolley_diameter_load)
+        self.e += np.sqrt(belt_length / 4 - np.pi / 8 * (self.trolley_diameter_drive + self.trolley_diameter_load)**2 - (self.trolley_diameter_drive + self.trolley_diameter_load)**2 / 8)
+        self.wrap_angle = np.arccos((self.trolley_diameter_load - self.trolley_diameter_drive) / (2 * e))
+        self.x = 0.005 * belt_length
+        return self.e, self.wrap_angle, self.x
+
+    def step_3_calc_module(self):
+        zk_ = 12 / np.rad2deg(self.wrap_angle) * 360
+        pitch_ = self.trolley_diameter_drive * np.pi / zk_
+        self.index = np.where(_TOOTHBELT_PARAMS_[:,0] >= pitch_)[0]
+        self.belt_params = _TOOTHBELT_PARAMS_[self.index]
+        self.zk = self.trolley_diameter_drive * self.belt_params[0] / np.pi
+        self.ze = self.zk * np.rad2deg(self.wrap_angle) / 360
+        return self.ze, self.zk, self.belt_params[0]
+
+    def step_4_calc_beltwidth(self, P_spez: float|int):
+        self.P_spez = P_spez
+        belt_width_ = self.power / (self.zk * self.ze * P_spez)
+        return belt_width_
+
+    def step_5_control_calculation(self, belt_width):
+        self.belt_width = belt_width
+        Ft_zul = _TOOTHBELT_FORCE_[self.index]
+        Ft_range = Ft_zul[np.where(_TOOTHBELT_FORCE_[0] >= self.belt_width)]
+        return Ft_range / self.tangential_force
 
 
 class FlatBelt(Belt):
     def __init__(self):
         pass
+
